5e18d39a1481430553f5673f70cca5069dff6f12
Raven <rdev1163@outlook.com>
2018-01-24 20:54:53 -0800

    added indexed-db-utilities.d.ts

--- src/indexed-db-utilities.d.ts
+++ src/indexed-db-utilities.d.ts
@@ -1,8 +1,4 @@
-import {openIDBUtilities} from "./utilities/index-db.utility";
 import {IDBUConfigModel} from "./models/config.model";
 import {IDBUtility} from "./models/idb-utility.model";
-
-declare var window;
-window.openIDBUtilities = openIDBUtilities;
 
 export declare const openIDBUtulities: (config: IDBUConfigModel) => IDBUtility;
